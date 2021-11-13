9d3ea7580766578b2f09b7c4af1708ad9eb32423
guilhermedonizetti <guilhermetecnologias@gmail.com>
2021-11-13 14:07:19 -0300

    Primeiro commit desse repositorio.

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+from functions import functions as fc
+
+app = Flask(__name__)
+
+@app.route("/")
+def inicial():
+    posicao = [1, 1]
+    return render_template("index.html", posicoes=posicao)
+
+@app.route("/ligar_pontos", methods=['POST'])
+def ligar_pontos():
+    coordenadas = []
+    coordenadas.append(request.form["X_1"])
+    coordenadas.append(request.form["Y_1"])
+    coordenadas.append(request.form["X_2"])
+    coordenadas.append(request.form["Y_2"])
+
+    for i in range(len(coordenadas)):
+        try:
+            coordenadas[i] = int(coordenadas[i])
+        except:
+            coordenadas[i] = 1
+
+    posicao = fc.ligar_os_pontos(coordenadas)
+    caminho = fc.buscar_caminho(posicao)
+
+    if caminho == False:
+        caminho = [0]
+        return render_template("index.html", posicoes=caminho)
+    else:
+        return render_template("index.html", posicoes=caminho)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ functions/functions.py
@@ -0,0 +1,28 @@
+from waypy.agent import Agente
+from functions import posicoes
+
+def ligar_os_pontos(coordenadas):
+    posicoes = []
+
+    pos1 = ((coordenadas[1] - 1) * 8) + coordenadas[0]
+    pos2 = ((coordenadas[3] - 1) * 8) + coordenadas[2]
+
+    posicoes.append(pos1)
+    posicoes.append(pos2)
+
+    print("{} {}".format(pos1, pos2))
+
+    return posicoes
+
+def buscar_caminho(pontos):
+    agente = Agente()
+    agente.starting_points = str(pontos[0]) #ponto de partida
+    agente.nodes = posicoes.nos
+    agente.graphs = posicoes.arestas
+
+    caminho = agente.encontrar_ajuda_humanitaria(str(pontos[1]), "AMPLITUDE") #ponto final, metodo
+
+    if type(caminho) != list:
+        return False
+    else:
+        return caminho
