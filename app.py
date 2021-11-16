4d4390b32a86d69fa769cad7cace32ac2c9702ed
guilhermedonizetti <guilhermetecnologias@gmail.com>
2021-11-16 08:39:44 -0300

    Comentarios no codigo.

--- app.py
+++ app.py
@@ -5,32 +5,35 @@ app = Flask(__name__)
 
 @app.route("/")
 def inicial():
+    #Retornar a pagina inicial com as posicoes predefinidas em 1 e 1.
     posicao = [1, 1]
     return render_template("index.html", posicoes=posicao)
 
 @app.route("/ligar_pontos", methods=['POST'])
 def ligar_pontos():
+    #Pegar as coordenadas X, Y do ponto de partida e de chegada:
     coordenadas = []
     coordenadas.append(request.form["X_1"])
     coordenadas.append(request.form["Y_1"])
     coordenadas.append(request.form["X_2"])
     coordenadas.append(request.form["Y_2"])
 
+    #Garantir que todos os valores sejam INT, se nao atribuir 1 por padrao:
     for i in range(len(coordenadas)):
         try:
             coordenadas[i] = int(coordenadas[i])
         except:
             coordenadas[i] = 1
 
+    #Obter os pontos dentro do quadrado e gerar caminho entre eles.
     posicao = fc.ligar_os_pontos(coordenadas)
-    print("Posicoes: {}".format(posicao))
     caminho = fc.buscar_caminho(posicao)
 
+    #Retornar a pagina inicial com os pontos do caminho, ou retorna uma lista com 0 (vai mostrar nada).
     if caminho == False:
         caminho = [0]
         return render_template("index.html", posicoes=caminho)
     else:
-        print("Caminho: {}".format(caminho))
         return render_template("index.html", posicoes=caminho)
 
 if __name__ == '__main__':
--- functions/functions.py
+++ functions/functions.py
@@ -4,6 +4,7 @@ from functions import posicoes
 def ligar_os_pontos(coordenadas):
     posicoes = []
 
+    #Calcula as coordenadas para saber a qual quadrado se referem.
     pos1 = ((coordenadas[1] - 1) * 8) + coordenadas[0]
     pos2 = ((coordenadas[3] - 1) * 8) + coordenadas[2]
 
@@ -15,12 +16,10 @@ def ligar_os_pontos(coordenadas):
 def buscar_caminho(pontos):
     agente = Agente()
     agente.starting_points = [str(pontos[0])] #ponto de partida
-    print("P0: {}".format(pontos[0]))
     agente.nodes = posicoes.nos
     agente.graphs = posicoes.arestas
 
-    caminho = agente.encontrar_ajuda_humanitaria(str(pontos[1]), "AMPLITUDE") #ponto final, metodo
-    print("P1: {}".format(pontos[1]))
+    caminho = agente.encontrar_ajuda_humanitaria(str(pontos[1]), "AMPLITUDE") #ponto final, metodo.
 
     if type(caminho) != list:
         return False
